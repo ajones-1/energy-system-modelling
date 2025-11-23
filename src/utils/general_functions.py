1f7cdd8ad618ceaf717622281beeaaec69161386
Alexander Jones <ajones215215@gmail.com>
2025-11-23 18:35:27 +0000

    add utils

---
+++ src/utils/general_functions.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import pandas as pd
+from loguru import logger
+
+def get_repo_root():
+    return Path(__file__).parent.parent.parent
+
+def load_data(url: str, file_path: str, use_cache: bool = True) -> pd.DataFrame:
+    """
+    Loads data from a URL or from a cached file.
+
+    Args:
+        url (str): The URL to download the data from.
+        file_path (str): The path of the cached file.
+        use_cache (bool): Whether to use the cached file if it exists.
+
+    Returns:
+        pd.DataFrame: The loaded data.
+    """
+    if use_cache:
+        try:
+            data = pd.read_csv(f"{file_path}")
+            logger.info(f"Loaded cached data from {file_path}.")
+            return data
+        except FileNotFoundError:
+            logger.info(f"No cached data found for {file_path}, downloading...")
+
+    # Download data (either use_cache=False or cache loading failed)
+    logger.info(f"Downloading data from {url}...")
+    data = pd.read_csv(url, index_col=[0, 1])
+    data.to_csv(f"{file_path}")
+    logger.info(f"Saved data to {file_path}.")
+
+    return data
